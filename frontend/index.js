6b42387925c97970cbfcdcfcccf14da150264830
Jack Hubert <jphubert93@gmail.com>
2021-01-27 16:20:03 -0700

    updated index with login information

--- frontend/index.js
+++ frontend/index.js
@@ -1,24 +1,50 @@
-console.log("JavaScript Started!")
+const addTrainerForm = document.getElementById("add-trainer");
+const addTrainerButton = document.getElementById("add-trainer-submit");
 
-fetch("https://pokeapi.co/api/v2/pokemon/382/")
+console.log(addTrainerForm.name.textContent)
+
+addTrainerForm.addEventListener('submit', event => {
+  event.preventDefault();
+  
+  const addTrainerFormData = new FormData(event.target);
+  const trainerName = addTrainerFormData.get('name')
+  const trainerGender = addTrainerFormData.get('gender')
+
+  fetch("http://localhost:3000/trainers", {
+      method: "POST",
+      headers: {
+        'Content-type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({ trainer: { name: trainerName, gender: trainerGender }})
+  })
     .then( response => response.json() )
-    .then( pokemon => {
-        console.log(pokemon)
-
-        p = document.createElement('p')
-        spriteImage = document.createElement('img')
-
-        typeName = document.createElement('a')
-
-
-        spriteImage.src = (pokemon.sprites.front_default)
-        p.textContent = pokemon.moves[0].move.name
-
-        pokemon
-
-        document.body.appendChild(spriteImage)
-        document.body.appendChild(p)
-    })
+    .then( trainer => 
+            document.location.href = `http://localhost:3001/trainers?id=${trainer.id}`
+        )
 
 
 
+  event.target.reset()
+})
+
+
+// document.location.href = `http://localhost:3001/trainers?name=${loginForm.name}`
+    // document.location.href = `www.google.com`
+
+// const loginForm = document.getElementById("login-form");
+// const loginButton = document.getElementById("login-form-submit");
+// const loginErrorMsg = document.getElementById("login-error-msg");
+
+// loginButton.addEventListener("click", (e) => {
+//     e.preventDefault();
+//     const username = loginForm.username.value;
+//     const password = loginForm.password.value;
+
+//     if (username === "user" && password === "web_dev") {
+//         alert("You have successfully logged in.");
+//         location.reload();
+//     } else {
+//         loginErrorMsg.style.opacity = 1;
+//     }
+// })
--- frontend/pokemon.js
+++ frontend/pokemon.js
@@ -22,3 +22,30 @@ const capitalize = (s) => {
     if (typeof s !== 'string') return ''
     return s.charAt(0).toUpperCase() + s.slice(1)
     }
+
+
+
+//     console.log("JavaScript Started!")
+
+// fetch("https://pokeapi.co/api/v2/pokemon/382/")
+//     .then( response => response.json() )
+//     .then( pokemon => {
+//         console.log(pokemon)
+
+//         p = document.createElement('p')
+//         spriteImage = document.createElement('img')
+
+//         typeName = document.createElement('a')
+
+
+//         spriteImage.src = (pokemon.sprites.front_default)
+//         p.textContent = pokemon.moves[0].move.name
+
+//         pokemon
+
+//         document.body.appendChild(spriteImage)
+//         document.body.appendChild(p)
+//     })
+
+
+
