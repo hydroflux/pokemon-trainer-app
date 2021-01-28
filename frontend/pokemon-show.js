534b5bb39f78a295c273450903a24ed831ebe1b6
coremand <valinevite@gmail.com>
2021-01-28 11:18:32 -0700

    show page

---
+++ frontend/pokemon-show.js
@@ -0,0 +1,11 @@
+const $pokemon = document.querySelector(".card")
+
+//fetch("http://localhost:4000/pokemons")
+//.then(response => response.json())
+//.then((res) => {
+    // res.forEach(pokemon => {
+     //   let showitem = document.createElement("img")
+    //    showitem.setAttribute("src",`${pokemon.image}`)
+    //    $pokemon.appendChild(showitem)
+   // })
+//});
